4f98313c9d2bb814d537f91d74eab64673cdb7e5
Fred Crary <fred@crary.us>
2017-05-03 16:03:13 -0700

    HW4 add about, toolbar

--- app/src/main/java/com/example/fredcrary/ad_340_project/MainActivity.java
+++ app/src/main/java/com/example/fredcrary/ad_340_project/MainActivity.java
@@ -2,24 +2,35 @@ package com.example.fredcrary.ad_340_project;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.fredcrary.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar appToolBar = (Toolbar) findViewById(R.id.commonToolbar);
+        appToolBar.inflateMenu(R.menu.toolbar);
+        setSupportActionBar(appToolBar);
 
         GridView gridview = (GridView) findViewById(R.id.mainGrid);
         gridview.setAdapter(new TextAdapter(this));
@@ -34,6 +45,25 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.aboutAction:
+                Intent intent = new Intent(MainActivity.this, AboutPage.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     /** Called when the user selects the SEND button */
