91e26280e2c4b091d270d2f18dee9189ede1d425
Fred Crary <fred@crary.us>
2017-05-09 19:35:00 -0700

    HW5 Listener & ErrorListener set up, error testing

--- app/src/main/java/com/example/fredcrary/ad_340_project/XmlActivity.java
+++ app/src/main/java/com/example/fredcrary/ad_340_project/XmlActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.android.volley.Cache;
 import com.android.volley.Network;
@@ -38,21 +39,29 @@ public class XmlActivity extends ToolBarClass {
         mRequestQueue = new RequestQueue(cache, network);       // Initiate the RequestQueue
         mRequestQueue.start();                                  // Start the queue
 
-        Log.d(TAG, "Request set up and started");
-        String url = "http://www.STARparent.com/AD340/AD340.xml";
+        String url = "http://www.STARparent.com/AD340/AD340a.xml";
 
         // Send the request and handle the response
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        Log.d(TAG, "Normal reponse from Volley");
+                        ((TextView) findViewById(R.id.xmlMsg)).setText(response);
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.d(TAG, "Error response from Volley");
+                        // from www.programcreek.com/java-api-examples/index.php
+                        String errMsg = "Volley returned an error:\n\n";
+                        if(error.networkResponse == null) {
+                            errMsg += error.getMessage();
+                        } else {
+                            errMsg += error.getMessage() + ", status "
+                                    + error.networkResponse.statusCode
+                                    + " - " + error.networkResponse.toString();
+                        }
+                        ((TextView) findViewById(R.id.xmlMsg)).setText(errMsg);
                     }
                 });
         mRequestQueue.add(stringRequest);
